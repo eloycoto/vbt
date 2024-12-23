f41878471eb9ac5fefc0043c8625336a0d1b1553
Eloy Coto <eloy.coto@acalustra.com>
2024-12-23 17:55:07 +0100

    next
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- firmware/src/accelerometer.rs
+++ firmware/src/accelerometer.rs
@@ -1,6 +1,8 @@
 #![allow(warnings)]
+use core::fmt::Write;
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::i2c::I2c;
+use heapless::Vec;
 use log::info;
 use micromath::F32Ext;
 
@@ -18,8 +20,17 @@ const ADXL345_DATAY1: u8 = 0x35;
 const ADXL345_DATAZ0: u8 = 0x36;
 const ADXL345_DATAZ1: u8 = 0x37;
 
+const WINDOW_SIZE: usize = 5;
+const CALIBRATION_SAMPLES: usize = 10;
 pub struct MotionDetector<I2C> {
     i2c: I2C,
+    baseline: Option<(f32, f32, f32)>,
+    samples: [(f32, f32, f32); WINDOW_SIZE],
+    sample_index: usize,
+    sample_count: usize,
+    motion_threshold: f32,
+    is_moving: bool,
+    last_position: DevicePosition,
 }
 
 impl<I2C> MotionDetector<I2C>
@@ -27,8 +38,18 @@ where
     I2C: I2c,
 {
     pub async fn new(i2c: I2C) -> Result<Self, I2C::Error> {
-        let mut detector = Self { i2c };
+        let mut detector = Self {
+            i2c,
+            baseline: None,
+            samples: [(0.0, 0.0, 0.0); WINDOW_SIZE],
+            sample_index: 0,
+            sample_count: 0,
+            motion_threshold: 0.1,
+            is_moving: false,
+            last_position: DevicePosition::Unknown,
+        };
         detector.power_on().await?;
+        detector.calibrate().await?;
         Ok(detector)
     }
 
@@ -62,5 +83,126 @@ where
         self.i2c.write(ADXL345_ADDR, &tx_buf).await?;
 
         Ok(())
+    }
+
+    pub async fn calibrate(&mut self) -> Result<(), I2C::Error> {
+        let mut sum = (0.0f32, 0.0f32, 0.0f32);
+
+        for _ in 0..CALIBRATION_SAMPLES {
+            let accel = self.get_accel().await?;
+            sum.0 += accel.0;
+            sum.1 += accel.1;
+            sum.2 += accel.2;
+        }
+
+        // Calculate average as baseline
+        self.baseline = Some((
+            sum.0 / CALIBRATION_SAMPLES as f32,
+            sum.1 / CALIBRATION_SAMPLES as f32,
+            sum.2 / CALIBRATION_SAMPLES as f32,
+        ));
+
+        Ok(())
+    }
+
+    pub async fn get_motion_state(&mut self) -> Result<MotionState, I2C::Error> {
+        let raw_accel = self.get_accel().await?;
+
+        self.samples[self.sample_index] = raw_accel;
+        self.sample_index = (self.sample_index + 1) % WINDOW_SIZE;
+        if self.sample_count < WINDOW_SIZE {
+            self.sample_count += 1;
+        }
+
+        let avg_accel = self.calculate_moving_average();
+
+        let relative_accel = match self.baseline {
+            Some(baseline) => (
+                avg_accel.0 - baseline.0,
+                avg_accel.1 - baseline.1,
+                avg_accel.2 - baseline.2,
+            ),
+            None => avg_accel,
+        };
+
+        let acceleration_magnitude =
+            (relative_accel.0.powi(2) + relative_accel.1.powi(2) + relative_accel.2.powi(2)).sqrt();
+
+        self.is_moving = acceleration_magnitude > self.motion_threshold;
+
+        // Determine position based on z-axis
+        let new_position = if relative_accel.2 > 0.5 {
+            DevicePosition::Down
+        } else if relative_accel.2 < -0.5 {
+            DevicePosition::Up
+        } else {
+            self.last_position.clone()
+        };
+
+        self.last_position = new_position.clone();
+
+        Ok(MotionState {
+            position: new_position,
+            is_moving: self.is_moving,
+            acceleration: avg_accel,
+            relative_acceleration: relative_accel,
+        })
+    }
+
+    fn calculate_moving_average(&self) -> (f32, f32, f32) {
+        let mut sum = (0.0, 0.0, 0.0);
+        let count = self.sample_count;
+
+        // Only average the number of samples we've actually collected
+        for i in 0..count {
+            sum.0 += self.samples[i].0;
+            sum.1 += self.samples[i].1;
+            sum.2 += self.samples[i].2;
+        }
+
+        (
+            sum.0 / count as f32,
+            sum.1 / count as f32,
+            sum.2 / count as f32,
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DevicePosition {
+    Up,
+    Down,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub struct MotionState {
+    pub position: DevicePosition,
+    pub is_moving: bool,
+    pub acceleration: (f32, f32, f32),
+    pub relative_acceleration: (f32, f32, f32),
+}
+
+impl MotionState {
+    pub fn is_up(&self) -> bool {
+        self.position == DevicePosition::Up
+    }
+
+    pub fn is_down(&self) -> bool {
+        self.position == DevicePosition::Down
+    }
+
+    pub fn get_acceleration(&self) -> (f32, f32, f32) {
+        self.acceleration
+    }
+
+    pub fn debug_str(&self) -> heapless::String<128> {
+        let mut s = heapless::String::new();
+        let _ = write!(
+            s,
+            "M{{pos:{:?},mov:{}}} acceleration: {:?}, relative_acceleration: {:?}",
+            self.position, self.is_moving, self.acceleration, self.relative_acceleration
+        );
+        s
     }
 }
